7ca4d0739d706462a73c4bbcca5f7eb1585c18d3
dohyun97 <dohyun647@gmail.com>
2023-05-17 16:45:10 -0400

    Token test

--- src/main/java/com/cos/jwt/filter/MyFilter1.java
+++ src/main/java/com/cos/jwt/filter/MyFilter1.java
@@ -9,12 +9,13 @@ import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
+
 @Slf4j
 public class MyFilter1 extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         log.info("Filter 1");
-        filterChain.doFilter(request,response); //계속 진행시키기 위해 이걸 넣어줘
+        filterChain.doFilter(request, response);
     }
-
 }
--- src/main/java/com/cos/jwt/filter/MyFilter3.java
+++ src/main/java/com/cos/jwt/filter/MyFilter3.java
@@ -8,11 +8,26 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
+
 @Slf4j
 public class MyFilter3 extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         log.info("Filter 3");
-        filterChain.doFilter(request,response);
+
+        if(request.getMethod().equals("POST")){
+            log.info("POST request");
+            String headerAuth = request.getHeader("Authorization"); //헤더 authorizaiton 값 가져와
+            //이 필터에서 토큰(헤더 authorizaiton 값)이 cos 일때만 controller로 진입하게 하고 그게아니면 진입 못하게 막아
+            if(headerAuth.equals("cos")){
+                filterChain.doFilter(request,response); //계속 진행시키기 위해 이걸 넣어줘
+            }else {
+                PrintWriter out = response.getWriter();
+                out.println("No Auth");
+            }
+        }
+
     }
+
 }
